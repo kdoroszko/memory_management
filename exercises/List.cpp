9de1e90ef38c681fea6acc52e1c1f42c85e72d4f
Krzysztof Doroszko <doroszko.k@gmail.com>
2019-09-01 13:56:48 +0200

    My solution of List.cpp

--- exercises/List.cpp
+++ exercises/List.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+#include <stdexcept>
 
 using namespace std;
 
@@ -10,85 +12,247 @@ public:
         value(v)
     {}
 
-    Node* next;
+    /*~Node(){
+        cout << "Hello from destructor :)" << '\n';
+        delete next;
+    }*/
+
+    //Node* next;
+    shared_ptr<Node> next;
     int value;
 };
+
+class EmptyListError : public runtime_error
+{
+public:
+    EmptyListError(const char* what_arg);
+    virtual const char* what() const noexcept;
+};
+
+EmptyListError::EmptyListError(const char* what_arg) : runtime_error(what_arg){}
+
+const char* EmptyListError::what() const noexcept
+{
+    return runtime_error::what();
+}
 
 class List
 {
 public:
     List();
-    void add(Node* node);
-    Node* get(const int value);
+    //void add(Node* node);
+    void add(shared_ptr<Node> node);
+    void add_at_front(shared_ptr<Node> node);
+    //Node* get(const int value);
+    shared_ptr<Node> get(const int value);
+    shared_ptr<Node> get_reverse(const int value);
+    void clear();
+
+    //~List(){ delete first;}
 
 private:
-    Node* first;
+    //Node* first;
+    shared_ptr<Node> first;
+    shared_ptr<Node> last;
+
+    bool it_is_not_the_last_node(shared_ptr<Node>& node);
+    bool this_node_has_not_a_next(shared_ptr<Node>& node);
+    bool it_is_not_the_node_from_list(shared_ptr<Node>& node);
+    void display_error_message_cant_add_twice_same_node(shared_ptr<Node>& node);
 };
 
 List::List() :
-    first(nullptr)
+    first(nullptr), last(nullptr)
 {}
 
-void List::add(Node* node)
+void List::add(/*Node**/ shared_ptr<Node> node)
 {
     if(!first)
     {
         first = node;
+        last = node;
     }
-    else
-    {
-        Node* current = first;
-        while(current->next)
+    else if(it_is_not_the_node_from_list(node))
         {
-            current = current->next;
+            //Node* current = first;
+            auto current = first;
+
+            while(current->next)
+            {
+                current = current->next;
+            }
+
+            current->next = node;
+            last = node;
         }
-        current->next = node;
-    }
+        else display_error_message_cant_add_twice_same_node(node);
 }
 
-Node* List::get(const int value)
+void List::add_at_front(shared_ptr<Node> node)
 {
     if(!first)
     {
-        cout << "List is empty!" << endl;
-        return nullptr;
+        first = node;
+        last = node;
+    }
+    else if(it_is_not_the_node_from_list(node))
+        {
+            auto temporary_ptr_to_node(first);
+            
+            first = node;
+            node->next = temporary_ptr_to_node;
+        }
+        else display_error_message_cant_add_twice_same_node(node);
+}
+
+//Node* List::get(const int value)
+shared_ptr<Node> List::get(const int value)
+{
+    if(!first)
+    {
+        //cout << "List is empty!" << '\n';
+        //return nullptr;
+
+        //throw runtime_error("List is empty!");
+        throw EmptyListError("List is empty!");
     }
     else
     {
-        Node* current = first;
+        //Node* current = first;
+        auto current = first;
         do
         {
             if(current->value == value)
             {
-                cout << "Found value " << current->value << endl;
+                cout << "Found value " << current->value << '\n';
                 return current;
             }
             else
             {
-                cout << "Going through " << current->value << endl;
+                cout << "Going through " << current->value << '\n';
                 current = current->next;
             }
         } while(current);
-        cout << "Not found: value " << value << endl;
+        cout << "Not found: value " << value << '\n';
         return nullptr;
     }
 }
 
+shared_ptr<Node> List::get_reverse(const int value)
+{
+    if(!first)
+        throw EmptyListError("List is empty!");
+    else
+    {
+        auto current = last;
+        do
+        {
+            if(current->value == value)
+            {
+                cout << "Found value " << current->value << '\n';
+                return current;
+            }
+            else
+            {
+                cout << "Going through " << current->value << '\n';
+
+                auto node_for_search = first;
+                while(current != node_for_search)
+                {
+                    if(node_for_search->next == current)
+                        current = node_for_search;
+                    else node_for_search = node_for_search->next;
+                }
+
+                if(current == first)
+                {
+                    if(current->value == value)
+                    {
+                        cout << "Found value " << current->value << '\n';
+                        return current;
+                    }
+                    else
+                        cout << "Going through " << current->value << '\n';
+                }
+            }
+        } while(current != first);
+        cout << "Not found: value " << value << '\n';
+        return nullptr;
+    }
+}
+
+void List::clear()
+{
+    first = nullptr;
+    last = nullptr;
+}
+
+bool List::it_is_not_the_last_node(shared_ptr<Node>& node)
+{
+    return node != last;
+}
+
+bool List::this_node_has_not_a_next(shared_ptr<Node>& node)
+{
+    return !node->next;
+}
+
+bool List::it_is_not_the_node_from_list(shared_ptr<Node>& node)
+{
+    return it_is_not_the_last_node(node) && this_node_has_not_a_next(node);
+}
+
+void List::display_error_message_cant_add_twice_same_node(shared_ptr<Node>& node)
+{
+    cout << "You can't add same node twice! (Node value: " << node->value << ")\n";
+}
+
 int main()
 {
-    List lista;
-    Node* node4 = new Node(4);
-    Node* node7 = new Node(7);
 
-    lista.add(node4);
-    lista.add(new Node(2));
-    lista.add(node7);
-    lista.add(new Node(9));
-    auto node = lista.get(1);
+    try
+    {
+        List lista;
+        //Node* node4 = new Node(4);
+        //Node* node7 = new Node(7);
+        //auto node_error_test = lista.get(1);
 
-    if (node)
-        cout << node->value << '\n';
+        auto node4 = make_shared<Node>(4);
+        auto node7 = make_shared<Node>(7);
+        auto node3 = make_shared<Node>(3);
+
+        lista.add(node4);
+        lista.add(make_shared<Node>(2));
+        lista.add(node7);
+        lista.add(make_shared<Node>(9));
+        lista.add(node3);
+        lista.add(node3);
+
+        auto node12 = make_shared<Node>(12);
+        lista.add_at_front(node12);
+        lista.add_at_front(node12);
+        lista.add_at_front(node7);
+        lista.add_at_front(node3);
+
+        auto node = lista.get(1);
+        node = lista.get_reverse(1);
+
+        lista.clear();
+        node = lista.get(1);
+
+        if (node)
+            cout << node->value << '\n';
+    }
+
+    catch(const EmptyListError& e)
+    {
+        cerr << "EmptyListError: " << e.what() << '\n';
+    }
+
+    /*catch(const runtime_error& e)
+    {
+        cerr << "Runtime_error: " << e.what() << '\n';
+    }*/
 
     return 0;
 }
-
